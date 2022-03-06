1bb2ed4608c77d37ba936d075478c71a9b7291df
Lewis-is-a-Git <101533222@student.swin.edu.au>
2022-03-06 20:53:50 +1100

    The project begins

---
+++ Bootchain/src/main/java/dev/lewisbh/Bootchain/BootchainApplication.java
@@ -0,0 +1,13 @@
+package dev.lewisbh.Bootchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootchainApplication.class, args);
+	}
+
+}
---
+++ Bootchain/src/test/java/dev/lewisbh/Bootchain/BootchainApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lewisbh.Bootchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
